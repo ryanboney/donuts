f669658733c8d36944cd51d0ff74b7e13767fbf9
ryanboney <ryanboney@gmail.com>
2023-11-28 14:45:48 -0500

    need autoclick

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const counter = document.getElementById("counter")
 const donut = document.getElementById("donut")
+const autoclicker = document.getElementById("autoclicker")
+const multiplier = document.getElementById("multiplier")
+
 let donuts = 0
 
 function addDonut(){
 donuts++;
 counter.innerHTML = `Donuts: ${donuts}`
+
+    if (donuts > 5){
+        autoclicker.innerHTML = '<img src="./images/clicker.png" alt="">WHERE YOU BUY AUTOCLICKER'
+    }
 }
 
-donut.addEventListener('click', addDonut); 
+donut.addEventListener('click', addDonut);
+
+
 
