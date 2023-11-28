8a996c4aea62aca899b498418b43a713c3ee3628
ryanboney <ryanboney@gmail.com>
2023-11-28 15:24:52 -0500

    good shape

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ const autoclicker = document.getElementById("autoclicker")
 const multiplier = document.getElementById("multiplier")
 
 let donuts = 0
+let autoclickEnabled = false
 
 function addDonut(){
-donuts++;
-counter.innerHTML = `Donuts: ${donuts}`
 
-    if (donuts > 5){
+    donuts++;
+    counter.innerHTML = `Donuts: ${donuts}`
+
+    if ((donuts > 5)&&(autoclickEnabled===false)){
         autoclicker.innerHTML = '<img src="./images/clicker.png" alt="">WHERE YOU BUY AUTOCLICKER'
+        autoclickEnabled = true
     }
+
 }
 
 donut.addEventListener('click', addDonut);
