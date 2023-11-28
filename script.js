c84a7088a8b8885c44ff3025528ce05e48476207
ryanboney <ryanboney@gmail.com>
2023-11-28 18:33:16 -0500

    small tweaks

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 const counter = document.getElementById("counter")
 const donut = document.getElementById("donut")
+
 const autoclicker = document.getElementById("autoclicker")
 const multiplier = document.getElementById("multiplier")
 
 let donuts = 0
 let autoclickEnabled = false
+let multiplierEnabled = false
+
+autoclicker.style.display="none"
+multiplier.style.display="none"
 
 function addDonut(){
 
     donuts++;
     counter.innerHTML = `Donuts: ${donuts}`
 
-    if ((donuts > 5)&&(autoclickEnabled===false)){
-        autoclicker.innerHTML = '<img src="./images/clicker.png" alt="">WHERE YOU BUY AUTOCLICKER'
+    if ((donuts > 3)&&(autoclickEnabled===false)){
+        autoclicker.innerHTML = '<img src="./images/clicker.png" alt="">UNLOCK AUTOCLICKER<br>(100 Donuts)<div id="reverse"><img src="./images/clicker.png" alt=""></div>'
         autoclickEnabled = true
+        autoclicker.style.display="";
+    }
+
+    if ((donuts > 3)&&(multiplierEnabled===false)){
+        multiplier.innerHTML = '<img src="./images/coin.png" alt="">UNLOCK MULTIPLIER<br>(100 Donuts)<div id="reverse"><img src="./images/coin.png" alt=""></div>'
+        multiplierEnabled = true
+        multiplier.style.display="";
     }
 
 }
 
+function donutAnim(){
+
+    
+
+}
+
+
 donut.addEventListener('click', addDonut);
+donut.addEventListener('click', donutAnim);
 
 
 
